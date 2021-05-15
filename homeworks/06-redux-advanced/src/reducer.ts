3ebf88262cb2dd1088a5c6d5e3cef162aaabea25
elegorod <elegorod@gmail.com>
2021-05-15 11:01:24 +0300

    H-06 Redux advanced homework: Switch for action types

--- homeworks/06-redux-advanced/src/reducer.ts
+++ homeworks/06-redux-advanced/src/reducer.ts
@@ -16,61 +16,77 @@ const pizzaComparator = (a: Pizza, b: Pizza) => a.name.localeCompare(b.name)
 
 export const reducer: Reducer<State, PizzaAction> = (state = { pizza: [], basket: [] }, action) => {
   let newState = state
-  if (action.type === PizzaActionType.LOAD_LIST) {
-    if (action.pizzas) {
-      newState = { ...state, pizza: action.pizzas}
-    }
-  } else if (action.type === PizzaActionType.ADD_TO_BASKET) {
-    const pizzaId = action.pizzaId
-    const pizza = state.pizza.find(p => p._id === pizzaId)
-    if (pizza) {
-      const pizzaInBasket = state.basket.find(p => p._id === pizzaId)
-      let newBasket
-      if (pizzaInBasket) {
-        newBasket = [
-          ...state.basket.filter(p => p._id !== pizzaId),
-          {
-            ...pizza,
-            count: pizzaInBasket.count + 1
-          }
-        ]
-      } else {
-        newBasket = [
-          ...state.basket,
-          {
-            ...pizza,
-            count: 1
-          }
-        ]
+  switch (action.type) {
+    case PizzaActionType.LOAD_LIST:
+      if (action.pizzas) {
+        newState = { ...state, pizza: action.pizzas }
       }
-      newBasket.sort(pizzaComparator)
-      newState = { ...state, basket: newBasket }
-    }
-  } else if (action.type === PizzaActionType.REMOVE_FROM_BASKET) {
-    const pizzaId = action.pizzaId
-    const pizza = state.pizza.find(p => p._id === pizzaId)
-    if (pizza) {
-      const pizzaInBasket = state.basket.find(p => p._id === pizzaId)
-      let newBasket
-      if (pizzaInBasket) {
-        const otherPizzas = state.basket.filter(p => p._id !== pizzaId)
-        if (pizzaInBasket.count > 1) {
-          newBasket = [
-            ...otherPizzas,
-            {
-              ...pizza,
-              count: pizzaInBasket.count - 1
-            }
-          ]
-        } else {
-          newBasket = otherPizzas
+      break
+    case PizzaActionType.ADD_TO_BASKET:
+      newState = addToBasket(state, action)
+      break
+    case PizzaActionType.REMOVE_FROM_BASKET:
+      newState = removeFromBasket(state, action)
+      break
+  }
+  return newState
+}
+
+function addToBasket(state: State, action: PizzaAction): State {
+  let newState = state
+  const pizzaId = action.pizzaId
+  const pizza = state.pizza.find(p => p._id === pizzaId)
+  if (pizza) {
+    const pizzaInBasket = state.basket.find(p => p._id === pizzaId)
+    let newBasket
+    if (pizzaInBasket) {
+      newBasket = [
+        ...state.basket.filter(p => p._id !== pizzaId),
+        {
+          ...pizza,
+          count: pizzaInBasket.count + 1
         }
-      } else {
-        newBasket = state.basket
-      }
-      newBasket.sort(pizzaComparator)
-      newState = { ...state, basket: newBasket }
+      ]
+    } else {
+      newBasket = [
+        ...state.basket,
+        {
+          ...pizza,
+          count: 1
+        }
+      ]
     }
+    newBasket.sort(pizzaComparator)
+    newState = { ...state, basket: newBasket }
+  }
+  return newState
+}
+
+function removeFromBasket(state: State, action: PizzaAction): State {
+  let newState = state
+  const pizzaId = action.pizzaId
+  const pizza = state.pizza.find(p => p._id === pizzaId)
+  if (pizza) {
+    const pizzaInBasket = state.basket.find(p => p._id === pizzaId)
+    let newBasket
+    if (pizzaInBasket) {
+      const otherPizzas = state.basket.filter(p => p._id !== pizzaId)
+      if (pizzaInBasket.count > 1) {
+        newBasket = [
+          ...otherPizzas,
+          {
+            ...pizza,
+            count: pizzaInBasket.count - 1
+          }
+        ]
+      } else {
+        newBasket = otherPizzas
+      }
+    } else {
+      newBasket = state.basket
+    }
+    newBasket.sort(pizzaComparator)
+    newState = { ...state, basket: newBasket }
   }
   return newState
 }
